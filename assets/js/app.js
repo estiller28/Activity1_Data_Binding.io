830c36d5f392e9c58c9312c842288df47ddd249a
Daniel Estiller <estillerdaniel@gmail.com>
2021-09-26 04:36:49 +0800

    edited some styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,13 @@
 
 
-// const assignment = Vue.createApp({
-//     data(){
-//         return{
-//             firstInput: '',
-//             secondInput: '',
-//             confirmedInput: ''
-//         };
-//     },
-//     methods: {
-        
-//     }
-// });
-
-// assignment.mount('#assignment');
-
 
 var assignment = new Vue({
     el: '#assignment',
     data:{
         firstInput: '',
         secondInput: '',
-        confirmedInput: ''
     },
+    
     methods:{
         submit(){
             alert('Form Submitted!');
